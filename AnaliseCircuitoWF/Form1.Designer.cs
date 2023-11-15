202fc4582b8b8e44094ab37ae8a66f8e56cde49e
Vitor Barbosa <vitor_barbosa23@outlook.com>
2023-11-15 14:15:41 -0300

    primeiro texto inserido

--- AnaliseCircuitoWF/Form1.Designer.cs
+++ AnaliseCircuitoWF/Form1.Designer.cs
@@ -28,21 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lb1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Font = new System.Drawing.Font("Montserrat SemiBold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lb1.Location = new System.Drawing.Point(10, 10);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(338, 21);
+            this.lb1.TabIndex = 0;
+            this.lb1.Text = "Digite quantas malhas possui o circuito escolhido:\r\n";
+            this.lb1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Font = new System.Drawing.Font("Montserrat SemiBold", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 21);
+            this.label1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(970, 644);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
